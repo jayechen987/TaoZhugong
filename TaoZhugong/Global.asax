b7fdd9af09ec640c8f4957a3b044d6bf449dcd45
JayeChen <jayechen987@gmail.com>
2020-02-28 13:32:29 +0800

    加入專案檔案。

---
+++ TaoZhugong/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaoZhugong.MvcApplication" Language="C#" %>
---
+++ TaoZhugong/Models/DbEntities/TaoZhugongDatabaseConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaoZhugong.Models.DbEntities
+{
+    public interface ITaoZhugongDatabaseConnection
+    {
+        IQueryable<Asset> QueryableAsset { get; }
+        IQueryable<Bookkeeping> QueryableBookkeeping { get; }
+        IQueryable<Dividends> QueryableDividends { get; }
+        IQueryable<Product> QueryableProduct { get; }
+        IQueryable<TransactionRecord> QueryableTransactionRecord { get; }
+        void Modified<T>(T model, EntityState entityState) where T : class;
+        int SaveChanges();
+    }
+
+    public class TaoZhugongDatabaseConnection : ITaoZhugongDatabaseConnection
+    {
+        TaoZhugongEntities db = new TaoZhugongEntities();
+
+        public IQueryable<Asset> QueryableAsset => db.Asset;
+        public IQueryable<Bookkeeping> QueryableBookkeeping => db.Bookkeeping;
+        public IQueryable<Dividends> QueryableDividends => db.Dividends;
+        public IQueryable<Product> QueryableProduct => db.Product;
+        public IQueryable<TransactionRecord> QueryableTransactionRecord => db.TransactionRecord;
+
+        public void Modified<T>(T model, EntityState entityState) where T : class
+        {
+            db.Entry(model).State = entityState;
+        }
+
+        public int SaveChanges()
+        {
+            return db.SaveChanges();
+        }
+    }
+}
