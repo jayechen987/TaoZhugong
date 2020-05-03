3132243f88fc3fc474ffe2639487b35ae34b9f65
JayeChen <jayechen987@gmail.com>
2020-05-03 11:52:23 +0800

    新增配股配息表頁面

---
+++ TaoZhugong/Controllers/DividendsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaoZhugong.Models.DbEntities;
+
+namespace TaoZhugong.Controllers
+{
+    public class DividendsController : Controller
+    {
+
+        // GET: Dividends
+        public ActionResult Index()
+        {
+            var list = new List<Dividends>();
+            return View(list);
+        }
+    }
+}
